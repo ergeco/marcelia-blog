b98d3a7e27b70d0b7f4244b390e9fccb5a705764
Maxim-Burkov <sup3r44ax@yandex.ru>
2023-09-26 23:41:26 +0300

    change url foк local running

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('marcelia', include('polls.urls')),
+    path('burkoff_blog', include('polls.urls')),
 
 ]
